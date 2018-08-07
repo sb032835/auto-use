5c071f8003afea6d0b90a09d68b6369c5aae6a6f
Tuturu <ujeshman@gmail.com>
2018-08-07 19:44:13 -0400

    Auto update

---
+++ README.md
@@ -0,0 +1,20 @@
+# Automatically uses Brooch and Rootbeer (if you have them) when using certain skills.  ***Auto detects the brooch you have***
+
+There is two section
+useBroochOn [Currently supports : Gamble, ARush, Dexter]
+useRootBeerOn [Currently supports: Gamble, ARush, Rampage]
+
+Put skills in respective field that you want to have auto use effect.
+
+You can get skill ID's from https://moongourd.com/info or do /proxy au debug and use the skill, it will print on your console.
+Commands: 
+> /proxy au on
+
+> /proxy au off
+
+> /proxy debug
+
+> /proxy help
+
+I have no idea when other classes uses their stuff so, add it yourself in skills!
+Note if you include multiple skill for same class, it will use brooch + beer on the first skill used where the items are off cooldown.
---
+++ index.js
@@ -0,0 +1,107 @@
+/*You can get skill ID's from https://moongourd.com/info or do /proxy au debug and use the skill, it will print on your console.
+Recommanded to note the skill name in same order so you don't get confused :3 */
+
+/* --------------------------------------------- NO EDITO BELOW THIS --------------------------------------------- */
+
+const Command = require('command');
+const GameState = require('tera-game-state');
+const skills = require('./skills');
+
+module.exports = function AutoUse(dispatch){
+const command = Command(dispatch);
+const game = GameState(dispatch);
+
+let enabled = true,
+	debug = false,
+	brooch = {
+		id : 0,
+		cooldown : 0
+	},
+	rootbeer = {
+		id : 80081,
+		amount : 0,
+		cooldown : 0
+	},
+	useBroochOn,
+	useRootBeerOn;
+
+	command.add('au', (arg) => {
+		if(arg){
+			arg = arg.toLowerCase();
+			if(arg === 'on'){
+				enabled = true;
+				command.message('[Auto Use] Enabled.');
+			}
+			else if(arg === 'off'){
+				enabled = false;
+				command.message('[Auto Use] Disabled.');
+			}
+			else if(arg === 'debug'){
+				debug = !debug;
+				command.message(`[Auto Use] Debug Status : ${debug}`);
+			}
+			else if(arg === 'help'){
+				command.message('[Auto Use] Commands : debug | on | off')
+			}
+		}
+		else command.message('[Auto Use] Commands : debug | on | off');
+	});
+
+	let useItem = (item, loc, w) => {
+		dispatch.toServer('C_USE_ITEM', 3, {
+			gameId: game.me.gameId,
+			id: item,
+			dbid: { low: 0, high: 0, unsigned: true },
+			target: { low: 0, high: 0, unsigned: true },
+			amount: 1,
+			dest: { x: 0, y: 0, z: 0 },
+			loc: loc,
+			w: w,
+			unk1: 0,
+			unk2: 0,
+			unk3: 0,
+			unk4: true
+        });
+        if(debug) console.log('Used : ' + item);
+	};
+
+	let handle = (info) => {
+		if(game.me.inCombat && !game.me.inBattleground){
+			if(useBroochOn.includes(info.skill.id) && Date.now() > brooch.cooldown) useItem(brooch.id, info.loc, info.w);
+			if(useRootBeerOn.includes(info.skill.id) && rootbeer.amount > 0 && Date.now() > rootbeer.cooldown) useItem(rootbeer.id, info.loc, info.w);
+		}
+	}; 
+
+	dispatch.game.on('enter_game', () => {
+        useBroochOn = skills[dispatch.game.me.class].useBroochOn;
+        useRootBeerOn = skills[dispatch.game.me.class].useRootBeerOn;
+    });
+
+ 	dispatch.hook('C_USE_ITEM', 3, event => {
+ 		if(debug) console.log('ID of Item Used: ' + event.id);
+ 	});
+
+	dispatch.hook('S_INVEN', 14, event => {
+		if(!enabled) return;
+		const broochinfo = event.items.find(item => item.slot === 20);
+		const beer = event.items.find(item => item.id === rootbeer.id);
+		if(broochinfo) brooch.id = broochinfo.id;
+		if(beer) rootbeer.amount = beer.amount;
+	});
+
+	dispatch.hook('C_START_SKILL', 6, event => {
+		if(debug){
+			const Time = new Date();
+			console.log('Time: ' + Time.getHours() + ':' + Time.getMinutes() + ' | Skill ID : ' + event.skill.id);
+		}
+		if(!enabled) return;
+		handle(event);
+	});
+
+	dispatch.hook('S_START_COOLTIME_ITEM', 1, event => {
+		if(!enabled) return;
+		if(event.item === brooch.id) brooch.cooldown = Date.now() + event.cooldown*1000;
+		else if(event.item === rootbeer.id) rootbeer.cooldown = Date.now() + event.cooldown*1000;
+	});
+
+}
