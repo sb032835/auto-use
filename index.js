e5841ab52688aa466e326e8bef6e188372cb99f0
sb032835 <sb032835@msn.com>
2018-11-09 17:26:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ Recommanded to note the skill name in same order so you don't get confused :3 */
 const Command = require('command'),
 	skills = require('./skills');
 
-module.exports = function AutoUse(dispatch){
-const command = Command(dispatch);
-
+module.exports = function AutoUse(mod){
 
 let enabled = true,
 	debug = false,
@@ -25,31 +23,31 @@ let enabled = true,
 	useRootBeerOn,
 	useOutOfCombat;
 
-	command.add('au', (arg) => {
+	mod.command.add('au', (arg) => {
 		if(arg){
 			arg = arg.toLowerCase();
 			if(arg === 'on'){
 				enabled = true;
-				command.message('[Auto Use] Enabled.');
+				mod.command.message('[Auto Use] Enabled.');
 			}
 			else if(arg === 'off'){
 				enabled = false;
-				command.message('[Auto Use] Disabled.');
+				mod.command.message('[Auto Use] Disabled.');
 			}
 			else if(arg === 'debug'){
 				debug = !debug;
-				command.message(`[Auto Use] Debug Status : ${debug}`);
+				mod.command.message(`[Auto Use] Debug Status : ${debug}`);
 			}
 			else if(arg === 'help'){
-				command.message('[Auto Use] Commands : debug | on | off')
+				mod.command.message('[Auto Use] Commands : debug | on | off')
 			}
 		}
-		else command.message('[Auto Use] Commands : debug | on | off');
+		else mod.command.message('[Auto Use] Commands : debug | on | off');
 	});
 
 	let useItem = (item, loc, w) => {
-		dispatch.toServer('C_USE_ITEM', 3, {
-			gameId: dispatch.game.me.gameId,
+		mod.toServer('C_USE_ITEM', 3, {
+			gameId: mod.game.me.gameId,
 			id: item,
 			dbid: { low: 0, high: 0, unsigned: true },
 			target: { low: 0, high: 0, unsigned: true },
@@ -66,23 +64,23 @@ let enabled = true,
 	};
 
 	let handle = (info) => {
-		if((useOutOfCombat || dispatch.game.me.inCombat) && !dispatch.game.me.inBattleground){
+		if((useOutOfCombat || mod.game.me.inCombat) && !mod.game.me.inBattleground){
 			if(useBroochOn.includes(info.skill.id) && Date.now() > brooch.cooldown) useItem(brooch.id, info.loc, info.w);
 			if(useRootBeerOn.includes(info.skill.id) && rootbeer.amount > 0 && Date.now() > rootbeer.cooldown) useItem(rootbeer.id, info.loc, info.w);
 		}
 	}; 
 
-	dispatch.game.on('enter_game', () => {
-        useBroochOn = skills[dispatch.game.me.class].useBroochOn;
-        useRootBeerOn = skills[dispatch.game.me.class].useRootBeerOn;
-        useOutOfCombat = skills[dispatch.game.me.class].useOutOfCombat;
+	mod.game.on('enter_game', () => {
+        useBroochOn = skills[mod.game.me.class].useBroochOn;
+        useRootBeerOn = skills[mod.game.me.class].useRootBeerOn;
+        useOutOfCombat = skills[mod.game.me.class].useOutOfCombat;
     });
 
- 	dispatch.hook('C_USE_ITEM', 3, event => {
+ 	mod.hook('C_USE_ITEM', 3, event => {
  		if(debug) console.log('ID of Item Used: ' + event.id);
  	});
 
-	dispatch.hook('S_INVEN', 14, event => {
+	mod.hook('S_INVEN', 14, event => {
 		if(!enabled) return;
 		const broochinfo = event.items.find(item => item.slot === 20);
 		const beer = event.items.find(item => item.id === rootbeer.id);
@@ -90,7 +88,7 @@ let enabled = true,
 		if(beer) rootbeer.amount = beer.amount;
 	});
 
-	dispatch.hook('C_START_SKILL', 6, {order: Number.NEGATIVE_INFINITY}, event => {
+	mod.hook('C_START_SKILL', 6, {order: Number.NEGATIVE_INFINITY}, event => {
 		if(debug){
 			const Time = new Date();
 			console.log('Time: ' + Time.getHours() + ':' + Time.getMinutes() + ' | Skill ID : ' + event.skill.id);
@@ -99,7 +97,7 @@ let enabled = true,
 		handle(event);
 	});
 
-	dispatch.hook('S_START_COOLTIME_ITEM', 1, {order: Number.NEGATIVE_INFINITY}, event => {
+	mod.hook('S_START_COOLTIME_ITEM', 1, {order: Number.NEGATIVE_INFINITY}, event => {
 		if(!enabled) return;
 		if(event.item === brooch.id) brooch.cooldown = Date.now() + event.cooldown*1000;
 		else if(event.item === rootbeer.id) rootbeer.cooldown = Date.now() + event.cooldown*1000;
